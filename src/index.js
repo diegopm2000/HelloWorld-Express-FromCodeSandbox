a303ff068f2811ad30fc28374f3c612e249db78c
Diego Perez Molinero <diegopm2000@gmail.com>
2021-02-21 16:31:28 +0100

    example example of creation of code & repository from codesandbox.io

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("Hello World Express");
+  res.send("Hello World Express, code created from codesandbox.io");
 });
 
 app.listen(8080);
